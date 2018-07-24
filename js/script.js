919697844fd9eb2038cc6109edf2014e475e1b5b
Ty Sweet <tysweet77@gmail.com>
2018-07-24 09:18:51 -0500

    updated to include foaas and statement

--- js/script.js
+++ js/script.js
@@ -12,3 +12,14 @@ function myFunction() {
     header.classList.remove('sticky');
   }
 }
+
+// $(document).scroll(function () {
+//   var x = $(this).scrollTop();
+//   if (x > 550) {
+//       $('#headerText').stop().fadeIn('2000');
+//       // console.log($('#headerText'))
+//   }
+//   // else{
+//   //     $('#headerText').stop().fadeOut('slow');
+//   // }
+// });
