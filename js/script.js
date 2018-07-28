48e49fff023e0567e330afb78af38150461389f6
Ty Sweet <tysweet77@gmail.com>
2018-07-27 21:55:54 -0500

    updated profile

--- js/script.js
+++ js/script.js
@@ -8,7 +8,13 @@ var sticky = header.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     header.classList.add('sticky');
+    $("h1").css({"font-size": "10em", "transition": "0.5s","transition-timing-function": "ease-in-out"});
+    $("h2").css({"font-size": "2em", "transition": "0.5s","transition-timing-function": "ease-in-out"});
+    $("h6").css({"font-size": "1em", "transition": "0.5s","transition-timing-function": "ease-in-out"});
   } else {
     header.classList.remove('sticky');
+    $("h1").css({"font-size": "18em", "transition": "1s","transition-timing-function": "ease-in-out"});
+    $("h2").css({"font-size": "3em", "transition": "1s","transition-timing-function": "ease-in-out"});
+    $("h6").css({"font-size": "2em", "transition": "1s","transition-timing-function": "ease-in-out"});
   }
 }
